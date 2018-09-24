b7aa277e8b8125f852b64eee6e7b6b00c7bb022c
tpshull <38360561+tpshull@users.noreply.github.com>
2018-09-24 01:32:09 -0400

    Add files via upload

--- web/connect.php
+++ web/connect.php
@@ -1,28 +1,18 @@
 <?php
-//Variables for Username, Password, Hostname
+	//Variables for Username, Password, Hostname
+	//Code for Our Server
+	$dbusername = 'c2facultytracker';
+	$dbpassword = '2015fTracker$';
 
-//Code for Our Server
-$dbusername = 'c2facultytracker';
-$dbpassword = '2015fTracker$';
+	//Variables for my local database on localhost
+	$hostname = 'localhost';
+	$databaseName = 'c2facultytracker';
 
-//Variables for my local database on localhost
-$hostname = 'localhost';
-
-$databaseName = 'c2facultytracker';
-
-//Code to connect to the database
-$con = mysqli_connect($hostname, $dbusername, $dbpassword, $databaseName);
-	
-//If it couldnt connect to the database, the error message will tell us why	
-if (!$con)
-{
-	die('Unable to connect to MySQL' . mysqli_error());
-}
-
-else
-{
-	//This selects the database	
-	$selected_db = mysqli_select_db($databaseName, $con);
-}
-
+	//Code to connect to the database
+	$conn = new mysqli($hostname, $dbusername, $dbpassword, $databaseName);
+		
+	//If it couldnt connect to the database, the error message will tell us why	
+	if ($conn->connect_error) {
+		die("DIE: Connection failed: " . $con->connect_error);
+	}
 ?>
